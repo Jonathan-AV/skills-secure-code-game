c780b6fb23fa006f6b75387839b86695fedd011c
Jonathan Alvarez <84749100+Jonathan-AV@users.noreply.github.com>
2024-07-12 14:40:48 +0000

    Update get_tax_form_attachment Season-1 Level-3

--- Season-1/Level-3/code.py
+++ Season-1/Level-3/code.py
@@ -28,12 +28,18 @@ class TaxPayer:
             pass
 
         # defends against path traversal attacks
-        if path.startswith('/') or path.startswith('..'):
-            return None
+        """ if path.startswith('/') or path.startswith('..'):
+            return None """
 
         # builds path
         base_dir = os.path.dirname(os.path.abspath(__file__))
         prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
+
+        """ print("--------------GET PROF PIC-------------------")
+        print("path: "+path)
+        print("base_dir: "+base_dir)
+        print("prof_picture: "+prof_picture_path)
+        print("common_path: "+os.path.commonpath([base_dir,prof_picture_path])) """
 
         if base_dir != os.path.commonpath([base_dir,prof_picture_path]):
             return None
@@ -52,12 +58,18 @@ class TaxPayer:
             raise Exception("Error: Tax form is required for all users")
         
         base_dir = os.path.dirname(os.path.abspath(__file__))
-        prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
+        filepath = os.path.normpath(os.path.join(base_dir, path))
 
-        if base_dir != os.path.commonpath([base_dir,prof_picture_path]):
+        """ print("--------------GET TAX FORM-------------------")
+        print("path: "+path)
+        print("base_dir: "+base_dir)
+        print("file_path: "+filepath)
+        print("common_path: "+os.path.commonpath([base_dir,filepath])) """
+
+        if base_dir != os.path.commonpath([base_dir,filepath]):
             return None
 
-        with open(path, 'rb') as form:
+        with open(filepath, 'rb') as form:
             tax_data = bytearray(form.read())
 
         # assume that tax data is returned on screen after this
